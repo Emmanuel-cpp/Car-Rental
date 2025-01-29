287aa3d8bc7b4904bd06fcaaded09b3da6cdf5b8
Emmanuel Siamoonga <emmanuelsiamoonga@gmail.com>
2025-01-29 13:00:20 +0200

    Second Commit

--- car_rental_project/settings.py
+++ car_rental_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home_app',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'car_rental_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/'template'],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_FILES_DIRS = (os.path.join(BASE_DIR,'static')),
+STATICFILES_DIRS = (os.path.join(BASE_DIR/'static')),
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
